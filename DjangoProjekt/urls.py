3dfc20f75fafcfa858092192fe6d1e3a10e0f5bd
IvanZimin <ivan.zimin.a@gmail.com>
2023-01-28 13:04:31 +0300

    Артём придурок

--- DjangoProjekt/urls.py
+++ DjangoProjekt/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from MainApp import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('',views.home, name="home"),
     path('about/',views.about, name="about"),
     path('items_list/',views.items_list, name="items-list"),
-    path('item/<int:id>',views.item_page, name="items-page"),
+    path('item/<int:id>',views.item_page, name="item_page"),
+    path('item/<int:id>', views.item_page, name= "item_page"),
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
